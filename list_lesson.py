d874d8232c428bbd686e46103975f55ca4aba877
ASKAR <askarjon.abdullayev@gmail.com>
2024-04-21 18:00:02 +0500

    list qo'shimcha dars

---
+++ list_lesson.py
@@ -0,0 +1,21 @@
+# fruits_l = ['apple', 'cherry', 'banana']
+# fruits_t = ('apple', 'banana', 'cherry')
+# mevalar = ['orange', 'melon', 'grapes']
+
+# # List bilan tuple farqi list o'zgaradi tuple o'zgarmaydi
+
+# fruits_l += ['pineapple']
+# print(fruits_l)
+
+
+# n = int(input())
+
+# match n:
+#     case 1: print("Dushanba")
+#     case 2: print("Seshanba")
+#     case 3: print("Chorshanba")
+#     case 4: print("Payshanba")
+#     case 5: print("Juma")
+#     case 6: print("Shanba")
+#     case 7: print("Yakshanba")
+#     case _: print("Bunday hafta kuni mavjud emas!")
